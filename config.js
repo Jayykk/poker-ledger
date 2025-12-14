36c14c804406ee3929ba480c442aaa96318d5991
Jay <zxcvbnm21329@hotmail.com>
2025-12-14 21:26:26 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+// 這裡只放設定資料，乾淨單純
+export const firebaseConfig = {
+    apiKey: "AIzaSyD1ftywMejmcdqzSXZ7CKhiJKMBeCrsyIQ",
+    authDomain: "poker-ledger-a0e06.firebaseapp.com",
+    projectId: "poker-ledger-a0e06",
+    storageBucket: "poker-ledger-a0e06.firebasestorage.app",
+    messagingSenderId: "825053137358",
+    appId: "1:825053137358:web:905bd41a2005a0f6cb039f",
+    measurementId: "G-7SBN8260L2"
+};
