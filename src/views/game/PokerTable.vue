bd8d6029be68e4825d368e95d8a96f3f55ea19c2
Jay <zxcvbnm21329@hotmail.com>
2025-12-19 19:42:31 +0000

    Change felt to rounded rectangle
    Override .poker-table-felt border-radius to 50px in the view wrapper.

--- src/views/game/PokerTable.vue
+++ src/views/game/PokerTable.vue
@@ -5,3 +5,10 @@
 <script setup>
 import PokerTable from '../../components/game/PokerTable.vue';
 </script>
+
+<style scoped>
+/* View-level override: switch the felt from oval to rounded rectangle. */
+:deep(.poker-table-felt) {
+  border-radius: 50px;
+}
+</style>
